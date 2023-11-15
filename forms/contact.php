fc6e14b7df284107f634ac12de4b511b18b18546
MoniqueRhodes86 <kuniquehill8617@gmail.com>
2023-11-15 17:59:02 +0530

    hey

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'praneeth0230@gmail.com.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -16,7 +16,7 @@
   }
 
   $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  $contact->ajax = FALSE;
   
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
